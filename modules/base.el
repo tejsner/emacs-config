7ebd5d53ca58d6a0ab5a73180202c4ea08349a37
tejsner <ttejsner@gmail.com>
2024-03-08 18:11:58 +0100

    remove unused avy+embark functionality

--- modules/base.el
+++ modules/base.el
@@ -34,22 +34,7 @@
 (use-package embark
   :ensure t
   :demand t
-  :after avy
-  :bind (("C-." . embark-act))        ; bind this to an easy key to hit
-  :init
-  ;; Add the option to run embark when using avy
-  (defun bedrock/avy-action-embark (pt)
-    (unwind-protect
-        (save-excursion
-          (goto-char pt)
-          (embark-act))
-      (select-window
-       (cdr (ring-ref avy-ring 0))))
-    t)
-
-  ;; After invoking avy-goto-char-timer, hit "." to run embark at the next
-  ;; candidate you select
-  (setf (alist-get ?. avy-dispatch-alist) 'bedrock/avy-action-embark))
+  :bind (("C-." . embark-act)))
 
 (use-package embark-consult
   :ensure t)
