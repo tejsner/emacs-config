7b07e87dbbbcb946d99b3dfe8cee0303d0d81c94
tejsner <ttejsner@gmail.com>
2024-03-30 11:40:26 +0100

    Add EMMS (music player) config

--- modules/multimedia.el
+++ modules/multimedia.el
@@ -17,3 +17,28 @@
   (add-hook 'nov-mode-hook 'visual-line-mode)
   (add-hook 'nov-mode-hook 'visual-fill-column-mode))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Music
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; this setup requires an MPD server running on localhost
+(use-package emms
+  :ensure t
+  :config
+  (require 'emms-setup)
+  (emms-all)
+  (add-to-list 'emms-info-functions 'emms-info-mpd)
+  (add-to-list 'emms-player-list 'emms-player-mpd)
+  (setq emms-player-mpd-server-name "localhost")
+  (setq emms-player-mpd-server-port "6600")
+  (setq emms-player-mpd-music-directory "~/data/music")
+  ;; remove mode line
+  (emms-mode-line nil)
+  (emms-playing-time nil)
+  ;; automatically connect and update cache
+  ;; (emms-player-mpd-connect)
+  ;; (emms-cache-set-from-mpd-all)
+  :bind
+  (("C-t m" . emms-smart-browse)))
