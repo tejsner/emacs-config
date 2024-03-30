8ea8a99c54b22083011733afa0a09ec96380a2b5
tejsner <ttejsner@gmail.com>
2024-03-30 14:36:08 +0100

    Fix volume buttons in EMMS

--- modules/multimedia.el
+++ modules/multimedia.el
@@ -23,18 +23,21 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; this setup requires an MPD server running on localhost
 (use-package emms
   :ensure t
   :config
   (require 'emms-setup)
   (emms-all)
+  ;; use mpd server on localhost
   (add-to-list 'emms-info-functions 'emms-info-mpd)
   (add-to-list 'emms-player-list 'emms-player-mpd)
   (setq emms-player-mpd-server-name "localhost")
   (setq emms-player-mpd-server-port "6600")
   (setq emms-player-mpd-music-directory "~/data/music")
-  ;; remove mode line
+  ;; volme buttons change on mpd server (+/- in playlist view)
+  (require 'emms-volume)
+  (setq emms-volume-change-function 'emms-volume-mpd-change)
+  ;; remove emms info on modeline
   (emms-mode-line nil)
   (emms-playing-time nil)
   ;; automatically connect and update cache
