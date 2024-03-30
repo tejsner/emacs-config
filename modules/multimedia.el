5ec5f5542c7e63e296af5aa0997c15c8d4966288
tejsner <ttejsner@gmail.com>
2024-03-30 10:11:05 +0100

    Fix visual-fill-column support in nov.el

--- modules/multimedia.el
+++ modules/multimedia.el
@@ -4,12 +4,16 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use-package visual-fill-column
+  :ensure t
+  :custom
+  (visual-fill-column-center-text t))
+
 (use-package nov
   :ensure t
   :config
   (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
   (setq nov-text-width t)
-  (setq visual-fill-column-center-text t)
   (add-hook 'nov-mode-hook 'visual-line-mode)
   (add-hook 'nov-mode-hook 'visual-fill-column-mode))
 
